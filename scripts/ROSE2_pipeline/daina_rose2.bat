816a4278292a8958b355d35c8804d50b9bb06469
eiserdr <49075191+eiserdr@users.noreply.github.com>
2020-03-16 16:11:06 -0400

    updated daina_rose2.bat to generic 'sample'

--- scripts/ROSE2_pipeline/daina_rose2.bat
+++ scripts/ROSE2_pipeline/daina_rose2.bat
@@ -1,36 +1,48 @@
 #!/bin/bash
-#script to run ROSE2 program and produce Super enhancer bed files.
+#script to run ROSE2 program and produce Super enhancer bed files. To submit:
 #sbatch --mem=10g daina_rose2.bat
-###index input and 27Ac bam files
+
 module load samtools R bamliquidator python/2.7
-#samtools index ../../../BamFiles/KP4_In_merged.sorted.bam
-#samtools index ../../../BamFiles/KP4_27Ac_1.sorted.bam
-#samtools index ../../../BamFiles/KP4_27Ac_2.sorted.bam
-#samtools index ../../../BamFiles/KP4_27Ac_merged.sorted.bam
+###Need to index input and 27Ac bam files
+samtools index ../../../BamFiles/Sample_In_merged.sorted.bam
+samtools index ../../../BamFiles/Sample_27Ac_1.sorted.bam
+samtools index ../../../BamFiles/Sample_27Ac_2.sorted.bam
+samtools index ../../../BamFiles/Sample_27Ac_merged.sorted.bam
 
 ###make bed file. It just needs a .bed extension
-mv ../../../out/Callpeak/Broadpeak/KP4_27Ac_1_filt_peaks.broadPeak ../../../out/Callpeak/Broadpeak/KP4_27Ac_1_filt_peaks.bed
-#mv ../../../out/Callpeak/Broadpeak/KP4_27Ac_2_filt_peaks.broadPeak ../../../out/Callpeak/Broadpeak/KP4_27Ac_2_filt_peaks.bed
-#mv ../../../out/Callpeak/Broadpeak/KP4_27Ac_merged_filt_peaks.broadPeak ../../../out/Callpeak/Broadpeak/KP4_27Ac_merged_filt_peaks.bed
-
-
-python ROSE2_main.py -g HG19 -i ../../../out/Callpeak/Broadpeak/KP4_27Ac_1_filt_peaks.bed -r ../../../BamFiles/KP4_27Ac_1.sorted.bam -c ../../../BamFiles/KP4_In_merged.sorted.bam -o ../../../out/ROSE2 -t 2500 -s 12500
+mv ../../../out/Callpeak/Broadpeak/Sample_27Ac_1_filt_peaks.broadPeak ../../../out/Callpeak/Broadpeak/Sample_27Ac_1_filt_peaks.bed
+mv ../../../out/Callpeak/Broadpeak/Sample_27Ac_2_filt_peaks.broadPeak ../../../out/Callpeak/Broadpeak/Sample_27Ac_2_filt_peaks.bed
+mv ../../../out/Callpeak/Broadpeak/Sample_27Ac_merged_filt_peaks.broadPeak ../../../out/Callpeak/Broadpeak/Sample_27Ac_merged_filt_peaks.bed
+###Run the main ROSE2 program
+python ROSE2_main.py -g HG19 -i ../../../out/Callpeak/Broadpeak/Sample_27Ac_1_filt_peaks.bed -r ../../../BamFiles/Sample_27Ac_1.sorted.bam -c ../../../BamFiles/Sample_In_merged.sorted.bam -o ../../../out/ROSE2 -t 2500 -s 12500
+### Remove extraneous files
 rm -r ../../../out/ROSE2/gff \
 ../../../out/ROSE2/mappedGFF
-
-#python ROSE2_main.py -g HG19 -i ../../../out/Callpeak/Broadpeak/KP4_27Ac_2_filt_peaks.bed -r ../../../BamFiles/KP4_27Ac_2.sorted.bam -c ../../../BamFiles/KP4_In_merged.sorted.bam -o ../../../out/ROSE2 -t 2500 -s 12500
-###Get rid of extraneous files
-#rm -r ../../../out/ROSE2/gff \
-#../../../out/ROSE2/mappedGFF
-###Make a bed file of just super enhancers.
-num_lines=$(wc -l < KP4_27Ac_merged_filt_peaks_Enhancers_withSuper.bed)
-super_lines=$(awk '/description=\"Super/ {print FNR}' KP4_27Ac_merged_filt_peaks_Enhancers_withSuper.bed)
+### Create a bed file of just super enhancers
+num_lines=$(wc -l < Sample_27Ac_1_filt_peaks_Enhancers_withSuper.bed)
+super_lines=$(awk '/description=\"Super/ {print FNR}' Sample_27Ac_1_filt_peaks_Enhancers_withSuper.bed)
 cut_off=$(($num_lines - $super_lines))
-tail -$cut_off KP4_27Ac_merged_filt_peaks_Enhancers_withSuper.bed > KP4_27Ac_merged_SuperEnhancers.bed
+tail -$cut_off Sample_27Ac_1_filt_peaks_Enhancers_withSuper.bed > Sample_27Ac_1_SuperEnhancers.bed
 
-#python ROSE2_main.py -g HG19 -i ../../../out/Callpeak/Broadpeak/KP4_27Ac_merged_filt_peaks.bed -r ../../../BamFiles/KP4_27Ac_merged.sorted.bam -c ../../../BamFiles/KP4_In_merged.sorted.bam -o ../../../out/ROSE2 -t 2500 -s 12500
-#rm -r ../../../out/ROSE2/gff \
-#../../../out/ROSE2/mappedGFF 
+python ROSE2_main.py -g HG19 -i ../../../out/Callpeak/Broadpeak/Sample_27Ac_2_filt_peaks.bed -r ../../../BamFiles/Sample_27Ac_2.sorted.bam -c ../../../BamFiles/Sample_In_merged.sorted.bam -o ../../../out/ROSE2 -t 2500 -s 12500
+###Get rid of extraneous files
+rm -r ../../../out/ROSE2/gff \
+../../../out/ROSE2/mappedGFF
+###Make a bed file of just super enhancers.
+num_lines=$(wc -l < Sample_27Ac_2_filt_peaks_Enhancers_withSuper.bed)
+super_lines=$(awk '/description=\"Super/ {print FNR}' Sample_27Ac_2_filt_peaks_Enhancers_withSuper.bed)
+cut_off=$(($num_lines - $super_lines))
+tail -$cut_off Sample_27Ac_2_filt_peaks_Enhancers_withSuper.bed > Sample_27Ac_2_SuperEnhancers.bed
+
+python ROSE2_main.py -g HG19 -i ../../../out/Callpeak/Broadpeak/Sample_27Ac_merged_filt_peaks.bed -r ../../../BamFiles/Sample_27Ac_merged.sorted.bam -c ../../../BamFiles/Sample_In_merged.sorted.bam -o ../../../out/ROSE2 -t 2500 -s 12500
+rm -r ../../../out/ROSE2/gff \
+../../../out/ROSE2/mappedGFF 
+num_lines=$(wc -l < Sample_27Ac_merged_filt_peaks_Enhancers_withSuper.bed)
+super_lines=$(awk '/description=\"Super/ {print FNR}' Sample_27Ac_merged_filt_peaks_Enhancers_withSuper.bed)
+cut_off=$(($num_lines - $super_lines))
+tail -$cut_off Sample_27Ac_merged_filt_peaks_Enhancers_withSuper.bed > Sample_27Ac_merged_SuperEnhancers.bed
+
+#Remove a few move extra files
 rm ../../../out/ROSE2/*12KB_STITCHED_TSS_DISTAL_ENHANCER_REGION_MAP.txt \
 ../../../out/ROSE2/*AllEnhancers* \
 ../../../out/ROSE2/*Enhancers_withSuperStretch.bed \
@@ -38,6 +50,7 @@ rm ../../../out/ROSE2/*12KB_STITCHED_TSS_DISTAL_ENHANCER_REGION_MAP.txt \
 ../../../out/ROSE2/*Enhancers_withStretch.bed \
 ../../../out/ROSE2/*StretchEnhancers.table.txt \
 ../../../out/ROSE2/*Plot_points_stretch.png
-#mv ../../../out/Callpeak/Broadpeak/KP4_27Ac_1_filt_peaks.bed ../../../out/Callpeak/Broadpeak/KP4_27Ac_1_filt_peaks.broadPeak
-#mv ../../../out/Callpeak/Broadpeak/KP4_27Ac_2_filt_peaks.bed ../../../out/Callpeak/Broadpeak/KP4_27Ac_2_filt_peaks.broaPeak
-#mv ../../../out/Callpeak/Broadpeak/KP4_27Ac_merged_filt_peaks.bed ../../../out/Callpeak/Broadpeak/KP4_27Ac_merged_filt_peaks.broadPeak
+###Chang ehte extension back to broadPeak
+mv ../../../out/Callpeak/Broadpeak/Sample_27Ac_1_filt_peaks.bed ../../../out/Callpeak/Broadpeak/Sample_27Ac_1_filt_peaks.broadPeak
+mv ../../../out/Callpeak/Broadpeak/Sample_27Ac_2_filt_peaks.bed ../../../out/Callpeak/Broadpeak/Sample_27Ac_2_filt_peaks.broadPeak
+mv ../../../out/Callpeak/Broadpeak/Sample_27Ac_merged_filt_peaks.bed ../../../out/Callpeak/Broadpeak/Sample_27Ac_merged_filt_peaks.broadPeak
